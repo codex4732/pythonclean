a54075190b57ba1043f64fc67e04d22af2df2e8f
codex4732 <jujudufrene@gmail.com>
2021-08-14 21:47:04 +0400

    changed to a ternary operator

--- hello.py
+++ hello.py
@@ -3,7 +3,5 @@ import Infrastructure as I
 drink = I.Services.DrinkService.getDrink()
 mood = I.Services.MoodService.getMood()
 
-if (mood == 'happy'):
-    print('I want to drink {0}'.format(drink))
-else:
-    print('I am not thirsty because I am {0}'.format(mood))
+
+print('I want to drink {0}'.format(drink)) if (mood == 'Happy') else print('I am not thirsty because I am {0}'.format(mood))
