69a0d12ea1d5a2f635eb59ee2ae7a10b9e19b5d2
codex4732 <jujudufrene@gmail.com>
2021-08-14 21:52:06 +0400

    changed Infrastructure to Interfaces

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-import Infrastructure as I
+import Interfaces as I
 
 drink = I.Services.DrinkService.getDrink()
 mood = I.Services.MoodService.getMood()
 
 
-print('I want to drink {0}'.format(drink)) if (mood == 'Happy') else print('I am not thirsty because I am {0}'.format(mood))
+print('I want to drink {0} because I am {1}'.format(drink, mood)) if (mood == 'Happy') else print('I am not thirsty because I am {0}'.format(mood))
